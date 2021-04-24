71983b637bdce1bbd107c4a421a43caa362dd7cd
Timothy Anido <xanido@gmail.com>
2021-04-24 16:30:00 +1000

    add barebones app to read and modify activeTab cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import { merge } from "lodash";
+import {
+  PrimaryButton,
+  DetailsListLayoutMode,
+  SelectionMode,
+  IDetailsRowProps,
+  DetailsRow,
+  DetailsList,
+} from "@fluentui/react";
+import { DeleteIcon } from "@fluentui/react-icons-mdl2";
+
+import logo from "./logo.svg";
+import cookieMonster from "./cookie-monster.png";
+import "./App.css";
+
+type KeyOfObject<O> = keyof O;
+type KnownCookie = KeyOfObject<typeof COOKIES>;
+
+const COOKIES = {
+  foo: "bar",
+  bar: "foo",
+  secret_auth_token: "topsecret",
+} as const;
 
 function App() {
+  const [cookies, setCookies] = useState<chrome.cookies.Cookie[]>([]);
+  const [activeUrl, setActiveUrl] = useState<URL>();
+  const activeDomain = activeUrl?.host;
+  const queue = useRef(Promise.resolve());
+
+  // read the current active tab url
+  useEffect(() => {
+    chrome.tabs &&
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const url = new URL(tabs[0].url as string);
+        setActiveUrl(url);
+      });
+  }, []);
+
+  // util to reload cookies for the active domain
+  const readCookiesIntoState = () => {
+    if (!activeDomain) return;
+    chrome.cookies.getAll(
+      {
+        domain: activeDomain,
+      },
+      (cookies_) => {
+        queue.current.then(() => setCookies(cookies_));
+      }
+    );
+  };
+
+  // immediately try to read the cookies
+  useEffect(readCookiesIntoState, [activeDomain]);
+
+  // handler factory - deletes a specific chrome.cookies.Cookie
+  // and then re-syncs the cookie state
+  const handleDeleteCookie = (cookie: any) => () => {
+    chrome.cookies.remove(
+      {
+        name: cookie.name,
+        url: `https://${cookie.domain}${cookie.path}`,
+      },
+      readCookiesIntoState
+    );
+  };
+
+  //handler factory - inject a known cookie from the COOKIES dict
+  const injectCookie = (cookieKey: KnownCookie) => () => {
+    chrome.cookies.set(
+      {
+        url: String(activeUrl),
+        name: cookieKey,
+        value: COOKIES[cookieKey],
+      },
+      readCookiesIntoState
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <main>
+        <PrimaryButton
+          menuProps={{
+            shouldFocusOnMount: true,
+            items: (Object.keys(COOKIES) as KnownCookie[]).map((name) => ({
+              key: name,
+              name: name,
+              onClick: injectCookie(name),
+            })),
+          }}
+        >
+          Inject cookie
+        </PrimaryButton>
+
+        <DetailsList
+          layoutMode={DetailsListLayoutMode.justified}
+          compact
+          items={cookies.map((cookie) => ({
+            name: cookie.name,
+            value: cookie.value,
+            cookie,
+          }))}
+          columns={[
+            {
+              key: "name",
+              name: "name",
+              isMultiline: false,
+              minWidth: 100,
+              maxWidth: 200,
+              fieldName: "name",
+            },
+            {
+              key: "value",
+              name: "value",
+              isMultiline: true,
+              minWidth: 250,
+              fieldName: "value",
+            },
+            {
+              key: "actions",
+              name: "",
+              onRender: (item, index, column) => {
+                console.log(item);
+                return <DeleteIcon onClick={handleDeleteCookie(item.cookie)} />;
+              },
+              minWidth: 24,
+              maxWidth: 24,
+            },
+          ]}
+          selectionMode={SelectionMode.single}
+          onRenderRow={(props?: IDetailsRowProps): JSX.Element => {
+            if (!props) {
+              return <DetailsRow item={undefined} itemIndex={-1} />;
+            }
+            const customStyles = merge(props.styles, {
+              root: {
+                userSelect: "any",
+              },
+            });
+            return <DetailsRow {...props} styles={customStyles} />;
+          }}
+        />
+      </main>
+
+      <footer>
+        <img src={cookieMonster} alt="Cookie Monsterrr" />
+      </footer>
     </div>
   );
 }
